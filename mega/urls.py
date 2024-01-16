7b706b36f31cfac4d3ee7dfad5f8e70c70bc2635
Tesla Telson <philipobiri3.1@gmail.com>
2024-01-16 15:27:32 +0000

    urls edited

--- mega/urls.py
+++ mega/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("hostel.urls")),
     path('', include("bookings.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
